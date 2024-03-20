8a44cb95ec1d5577e8a766ea790b82ecdb48ac53
Richard6141 <somasse-richard.salanon@epitech.eu>
2024-03-20 14:26:44 +0100

    modify arrondissement index

--- app/Http/Controllers/api/ArrondissementController.php
+++ app/Http/Controllers/api/ArrondissementController.php
@@ -23,50 +23,50 @@ class ArrondissementController extends Controller
      * @urlParam page int Le numero de page. Example: 3 
      * @return JsonResponse La liste des arrondissements.
      */
-    // public function index(ArrondissementRequest $request)
-    // {
-    //     try {
-    //         $arrondissements = Arrondissement::query();
-    //         if ($request->input('search') == null && $request->input('page')== null && $perPage = $request->input('perPage') == null) {
-    //             $result = $arrondissements->get();
-    //             return response()->json($result);
-    //         }
+    public function index(ArrondissementRequest $request)
+    {
+        try {
+            $arrondissements = Arrondissement::query();
+            if ($request->input('search') == null && $request->input('page')== null && $perPage = $request->input('perPage') == null) {
+                $result = $arrondissements->get();
+                return response()->json($result);
+            }
             
-    //         $search = $request->input('search');
-    //         $page = $request->input('page', 1);
-    //         $perPage = $request->input('perPage', 40);
+            $search = $request->input('search');
+            $page = $request->input('page', 1);
+            $perPage = $request->input('perPage', 40);
         
-    //         // Filtrer les arrondissements en fonction du terme de recherche s'il est fourni
-    //         if ($search) {
-    //             $arrondissements->where('label', 'LIKE', '%' . $search . '%');
-    //         }
-    //         // Récupérer le nombre total de résultats
-    //         $total = $arrondissements->count();
-    //         if ($total==0) {
-    //             return response()->json(["error" => "Arrondissement non trouvé"], 404);
-    //         }
-    //         // Calculer le nombre total de pages
-    //         $lastPage = ceil($total / $perPage);
-    //         // Vérifier si la page demandée est valide
-    //         if ($page < 1 || $page > $lastPage) {
-    //             return response()->json(["error" => "Aucun résultat sur cette page"], 404);
-    //         }
-    //         // Récupérer les arrondissements pour la page demandée
-    //         $result = $arrondissements->offset(($page - 1) * $perPage)->limit($perPage)->get();
-    //         // Vérifier si aucun résultat sur la page
-    //         if ($result->isEmpty()) {
-    //             return response()->json(["error" => "Aucun résultat sur cette page"], 404);
-    //         }
+            // Filtrer les arrondissements en fonction du terme de recherche s'il est fourni
+            if ($search) {
+                $arrondissements->where('label', 'LIKE', '%' . $search . '%');
+            }
+            // Récupérer le nombre total de résultats
+            $total = $arrondissements->count();
+            if ($total==0) {
+                return response()->json(["error" => "Arrondissement non trouvé"], 404);
+            }
+            // Calculer le nombre total de pages
+            $lastPage = ceil($total / $perPage);
+            // Vérifier si la page demandée est valide
+            if ($page < 1 || $page > $lastPage) {
+                return response()->json(["error" => "Aucun résultat sur cette page"], 404);
+            }
+            // Récupérer les arrondissements pour la page demandée
+            $result = $arrondissements->offset(($page - 1) * $perPage)->limit($perPage)->get();
+            // Vérifier si aucun résultat sur la page
+            if ($result->isEmpty()) {
+                return response()->json(["error" => "Aucun résultat sur cette page"], 404);
+            }
         
-    //         return response()->json([
-    //             "current_page" => $page,
-    //             "last_page" => $lastPage,
-    //             "arrondissements" => $result
-    //         ]);
-    //     } catch (Exception $e) {
-    //         return response()->json($e->getMessage(), 500);
-    //     }  
-    // }
+            return response()->json([
+                "current_page" => $page,
+                "last_page" => $lastPage,
+                "arrondissements" => $result
+            ]);
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 500);
+        }  
+    }
 
     /**
      * Récupère les quartiers d'un arrondissement d'une commune spécifique
