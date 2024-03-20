8257f308998797c88b78fe850941c5d865d47c3c
Richard6141 <somasse-richard.salanon@epitech.eu>
2024-03-20 14:29:34 +0100

    modify quartier index

--- app/Http/Controllers/api/QuartierController.php
+++ app/Http/Controllers/api/QuartierController.php
@@ -21,48 +21,48 @@ class QuartierController extends Controller
      * @urlParam page int Le numero de page. Example: 5
      * @return JsonResponse liste des quartiers.
      */
-    // public function index(QuartierRequest $request)
-    // {
-    //     try {
-    //         $quartiers = Quartier::query();
-    //         if ($request->input('search') == null && $request->input('page')== null && $perPage = $request->input('perPage') == null) {
-    //             $result = $quartiers->get();
-    //             return response()->json($result);
-    //         }
+    public function index(QuartierRequest $request)
+    {
+        try {
+            $quartiers = Quartier::query();
+            if ($request->input('search') == null && $request->input('page')== null && $perPage = $request->input('perPage') == null) {
+                $result = $quartiers->get();
+                return response()->json($result);
+            }
             
-    //         $search = $request->input('search');
-    //         $page = $request->input('page', 1);
-    //         $perPage = $request->input('perPage', 50);
+            $search = $request->input('search');
+            $page = $request->input('page', 1);
+            $perPage = $request->input('perPage', 50);
         
-    //         // Filtrer les quartiers en fonction du terme de recherche s'il est fourni
-    //         if ($search) {
-    //             $quartiers->where('label', 'LIKE', '%' . $search . '%');
-    //         }
-    //         // Récupérer le nombre total de résultats
-    //         $total = $quartiers->count();
-    //         if ($total==0) {
-    //             return response()->json(["error" => "Quartier non trouvé"], 404);
-    //         }
-    //         // Calculer le nombre total de pages
-    //         $lastPage = ceil($total / $perPage);
-    //         // Vérifier si la page demandée est valide
-    //         if ($page < 1 || $page > $lastPage) {
-    //             return response()->json(["error" => "Aucun résultat sur cette page"], 404);
-    //         }
-    //         // Récupérer les quartiers pour la page demandée
-    //         $result = $quartiers->offset(($page - 1) * $perPage)->limit($perPage)->get();
-    //         // Vérifier si aucun résultat sur la page
-    //         if ($result->isEmpty()) {
-    //             return response()->json(["error" => "Aucun résultat sur cette page"], 404);
-    //         }
+            // Filtrer les quartiers en fonction du terme de recherche s'il est fourni
+            if ($search) {
+                $quartiers->where('label', 'LIKE', '%' . $search . '%');
+            }
+            // Récupérer le nombre total de résultats
+            $total = $quartiers->count();
+            if ($total==0) {
+                return response()->json(["error" => "Quartier non trouvé"], 404);
+            }
+            // Calculer le nombre total de pages
+            $lastPage = ceil($total / $perPage);
+            // Vérifier si la page demandée est valide
+            if ($page < 1 || $page > $lastPage) {
+                return response()->json(["error" => "Aucun résultat sur cette page"], 404);
+            }
+            // Récupérer les quartiers pour la page demandée
+            $result = $quartiers->offset(($page - 1) * $perPage)->limit($perPage)->get();
+            // Vérifier si aucun résultat sur la page
+            if ($result->isEmpty()) {
+                return response()->json(["error" => "Aucun résultat sur cette page"], 404);
+            }
         
-    //         return response()->json([
-    //             "current_page" => $page,
-    //             "last_page" => $lastPage,
-    //             "quartiers" => $result
-    //         ]);
-    //     } catch (Exception $e) {
-    //         return response()->json($e->getMessage(), 500);
-    //     }  
-    // }
+            return response()->json([
+                "current_page" => $page,
+                "last_page" => $lastPage,
+                "quartiers" => $result
+            ]);
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 500);
+        }  
+    }
 }
